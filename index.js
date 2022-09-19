8dc17681a030bd01fed81333f1a0bd97c769d3ba
shyam <shyamvaranpal95060@gmail.com>
2022-09-19 23:57:23 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/*let colors=document.querySelectorAll('.color');
+for(let i=0; i<colors.length;i++){
+    colors[i].addEventListener('onclick',function(){
+        console.log('copied')
+    });
+}
+*/
+
+alert("script is running");
