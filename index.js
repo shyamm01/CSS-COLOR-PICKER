df235a1087c09d85b2ad918e06c0a4e4bf0f1216
shyam varan pal <69836193+shyamm01@users.noreply.github.com>
2022-10-19 00:30:21 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 /*let colors=document.querySelectorAll('.color');
-
+for(let i=0; i<colors.length;i++){
+    colors[i].addEventListener('onclick',function(){
+        console.log('copied')
+    });
+}
 */
 
+const colorCode = document.querySelectorAll('.color');
+
+colorCode[].addEventListener("click", () => {
+    console.log("copied")
+});
