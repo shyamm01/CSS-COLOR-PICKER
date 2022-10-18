9ce83af3a86519ecca539b9e62403bf7842d5e85
shyam varan pal <69836193+shyamm01@users.noreply.github.com>
2022-10-18 23:55:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 /*let colors=document.querySelectorAll('.color');
-for(let i=0; i<colors.length;i++){
-    colors[i].addEventListener('onclick',function(){
-        console.log('copied')
-    });
-}
+
 */
 
-alert("script is running");
